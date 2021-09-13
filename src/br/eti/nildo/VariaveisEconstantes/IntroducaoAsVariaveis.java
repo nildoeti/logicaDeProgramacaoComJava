3379574e3ee9959560ccddbe714c2d3642efa968
Nildo <nildo@webstack.com.br>
2021-09-13 11:39:08 -0300

    Atualizado  IntroducaoAsVariaveis.java

--- src/br/eti/nildo/VariaveisEconstantes/IntroducaoAsVariaveis.java
+++ src/br/eti/nildo/VariaveisEconstantes/IntroducaoAsVariaveis.java
@@ -3,7 +3,28 @@ package br.eti.nildo.VariaveisEconstantes;
 public class IntroducaoAsVariaveis {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		/* Variáveis são fundamentais para referenciar um valor na memória do 
+		 * computador. 
+		 * E para isso é preciso entender os tipos de dados que uma variável
+		 * pode armazenar.
+		 * 
+		 * 
+		 * Tipos Numéricos (Inteiros)
+		 * Byte
+		 * Short
+		 * Integer
+		 * Long
+		 * 
+		 * Tipos Numéricos (Decimais)
+		 * Float
+		 * Double
+		 * 
+		 * Tipo Texto
+		 * Char
+		 * String
+		 * 
+		 */
+		
 
 	}
 
