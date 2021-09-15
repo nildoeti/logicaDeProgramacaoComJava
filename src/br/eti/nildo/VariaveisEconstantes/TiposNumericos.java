9caa91d81e76b7a593b1b380a265eb195d9c7f2e
Nildo <nildo@webstack.com.br>
2021-09-14 21:01:15 -0300

    Adicioando TiposNumericos ao projeto.

---
+++ src/br/eti/nildo/VariaveisEconstantes/TiposNumericos.java
@@ -0,0 +1,25 @@
+package br.eti.nildo.VariaveisEconstantes;
+
+public class TiposNumericos {
+
+	public static void main(String[] args) {
+		/*
+		 * TIPOS numéricos: Byte, Short, Integer, Long, Float, Double
+		 * 
+		 * Os varios tipos de dados existem para a otimização principalmente
+		 * de memória.
+		 * 
+		 */
+		
+		// Tipos inteiros
+		Byte  tipoByte = -127; // armazena de -127 até 128 
+		Short tipoShort = -32768; // armazena de -32768 até 32767
+		Integer tipoInteger = -2147483648; // armazena de -2147483648 até 2147483647
+		Long tipoLong = 1L; //armazena de -9,223372037×10¹⁸ até 9,223372037×10¹⁸
+		
+		// Tipos float
+		Float numeroFloat = 1.0F;
+
+	}
+
+}
