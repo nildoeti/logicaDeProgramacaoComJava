12b631d64b9ec6705438e3686ab5e72979c9662f
Nildo <nildo@webstack.com.br>
2022-02-11 20:09:19 -0300

    adicioando CalculoIndiceMassaCorporal ao projeto

---
+++ src/logicaDeProgramacaoComJavaIntroducao/CalculoIndiceMassaCorporal.java
@@ -0,0 +1,15 @@
+package logicaDeProgramacaoComJavaIntroducao;
+
+import java.util.Scanner;
+
+public class CalculoIndiceMassaCorporal {
+
+	public static void main(String[] args) {
+		// FORMULA  IMC = PESO / (ALTURA * ALTURA)
+		Scanner terminal = new Scanner(System.in);
+		
+		terminal.close();
+
+	}
+
+}
